2de90869b5a717dd71f271bbc24371b67b09249f
Karl <toom.karl42@gmail.com>
2023-05-25 16:23:47 +0300

    useState done

--- App.jsx
+++ App.jsx
@@ -2,22 +2,51 @@ import "./App.css";
 import Todo from "./compontents/Todo.jsx";
 import Title from "./compontents/title.jsx";
 import Modal from "./compontents/Modal.jsx";
+import Counter from "./compontents/Counter.jsx";
+import React, { useState } from "react";
+
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function onTodoDelete() {
+    setShowModal(true);
+    console.log("onTodoDelete()");
+  }
+  function cancelModal() {
+    setShowModal(false);
+  }
+
+  function confirmModal() {
+    setShowModal(false);
+  }
+
   return (
     <div>
       <Title />
       <div>
-        <input type="text" onChange={(event) =>{
-console.log(event.target.value)
-        }} />
-        <button>Add todo</button>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button onClick={() => setShowModal(true)}>Add todo</button>
       </div>
       <div className="todo__wrapper">
-        <Todo title="Finish frontend Simplified" />
-        <Todo title="complete the interview section" />
-        <Todo title="Land a Job" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish frontend Simplified" />
+        <Todo
+          onTodoDelete={onTodoDelete}
+          title="complete the interview section"
+        />
+        <Todo onTodoDelete={onTodoDelete} title="Land a Job" />
       </div>
-      {<Modal title="Are you sure you want do delete?" />}
+      {showModal ? (
+        <Modal
+          cancelModal={cancelModal}
+          confirmModal={confirmModal}
+          title="confirm Delete?"
+        />
+      ) : null}
     </div>
   );
 }
--- compontents/Todo.jsx
+++ compontents/Todo.jsx
@@ -1,12 +1,9 @@
 import "./Todo.css";
-function Todo({ title}) {
-  function deleteTodo(id){
-    console.log(`deleteTodo()`, title)
-  }
+function Todo({ title, onTodoDelete}) {
   return (
     <div className="todo">
       <p>{title}</p>
-      <button onClick={() =>deleteTodo(1)}>Delete</button>
+      <button onClick={onTodoDelete}>Delete</button>
     </div>
   );
 }
