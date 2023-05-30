9f4ad9c4f8711575a644f16fb2efb0c3c2b83731
Karl <toom.karl42@gmail.com>
2023-05-30 17:33:18 +0300

    React Crash Course completed

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,5 +1,43 @@
-function Home(){
-    return <h1>Home</h1>
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import User from "../compontents/User";
+import { Link } from "react-router-dom";
+import { render } from "@testing-library/react";
+
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    setUsers(data);
+    console.log(data);
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      fetchUsers();
+    }, 1000);
+  }, []);
+
+  function renderUsers(){
+return users.map((user) => (
+  <Link to={`/users/${user.id}`} key={user.id}>
+    <User
+      id={user.id}
+      name={user.name}
+      email={user.email}
+      username={user.username}
+    />
+  </Link>
+))
+  }
+
+  return (
+    <div>
+      {users.length ? renderUsers() : <h1>Loading...</h1>}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
